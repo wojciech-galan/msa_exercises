dc721a7f97fa5fc3451bfa46a156d3e93ec26b5b
wojciech-galan <wojciech.galan@gmail.com>
2018-10-17 14:09:38 +0200

    baliscore wrapper

--- code/Wrappers.py
+++ code/Wrappers.py
@@ -4,7 +4,9 @@
 import abc
 import subprocess
 
-class MSAToolWrapper(abc.ABC):
+from code.Parsers import parse_blaliscore_v2_output
+
+class ToolWrapper(abc.ABC):
     @abc.abstractmethod
     def __init__(self):
         super().__init__()
@@ -14,7 +16,7 @@ class MSAToolWrapper(abc.ABC):
         command = self.command.format(**{'infile':infile, 'outfile':outfile})
         subprocess.run(command, shell=True, stdout=subprocess.PIPE)
 
-class ClustalWWrapper(MSAToolWrapper):
+class ClustalWWrapper(ToolWrapper):
     command = 'clustalw -INFILE={infile} -OUTPUT=GCG -OUTFILE={outfile}'
     def __init__(self):
         super().__init__()
@@ -22,7 +24,7 @@ class ClustalWWrapper(MSAToolWrapper):
     def run(self, infile:str, outfile:str):
         super().run(infile, outfile)
 
-class ClustalOWrapper(MSAToolWrapper):
+class ClustalOWrapper(ToolWrapper):
     command = 'clustalo -i {infile} --outfmt=msf -o {outfile} --force'
     def __init__(self):
         super().__init__()
@@ -30,7 +32,7 @@ class ClustalOWrapper(MSAToolWrapper):
     def run(self, infile:str, outfile:str):
         super().run(infile, outfile)
 
-class MuscleWrapper(MSAToolWrapper):
+class MuscleWrapper(ToolWrapper):
     command = 'muscle -in {infile} -msf -out {outfile} -QUIET'
     def __init__(self):
         super().__init__()
@@ -38,9 +40,21 @@ class MuscleWrapper(MSAToolWrapper):
     def run(self, infile:str, outfile:str):
         super().run(infile, outfile)
 
+class BaliScoreWrapper(object):
+    command = 'bali_score_old {reference_alignment} {test_alignment} '
+    def __init__(self):
+        super().__init__()
+
+    def run(self, reference_alignment:str, test_alignment:str):
+        command = self.command.format(**{'reference_alignment':reference_alignment, 'test_alignment':test_alignment})
+        output = subprocess.check_output(command, shell=True)#, stdout=subprocess.PIPE)
+        return parse_blaliscore_v2_output(output)
+
 
 if __name__ == '__main__':
     cw = MuscleWrapper()
     import os
     print(os.listdir('.'))
     cw.run('data/ref_1.2/BB12001.tfa', 'temp/test.out')
+    bs = BaliScoreWrapper()
+    print(bs.run('data/ref_1.2/BB12001.msf', 'temp/test.out'))
