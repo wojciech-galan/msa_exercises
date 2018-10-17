c2014237ddb7cdc671fbd222f6a8d64dbff5b9a8
wojciech-galan <wojciech.galan@uj.edu.pl>
2018-10-17 17:16:13 +0200

    raises exception when cannot find proper sp and/or tc values

--- code/Parsers.py
+++ code/Parsers.py
@@ -6,8 +6,13 @@ import re
 SP_RE = re.compile("SP score= ([0-9.]+)")
 TC_RE = re.compile("TC score= ([0-9.]+)")
 
+class NotProperNumberException(Exception):
+    pass
 
 def parse_blaliscore_v2_output(text):
-    sp = SP_RE.search(text.decode("utf-8")).group(1)
-    tc = TC_RE.search(text.decode("utf-8")).group(1)
+    try:
+        sp = SP_RE.search(text.decode("utf-8")).group(1)
+        tc = TC_RE.search(text.decode("utf-8")).group(1)
+    except AttributeError:
+        raise NotProperNumberException()
     return float(sp), float(tc)
