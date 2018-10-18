11cc62b4aec0d8ae2128fef675383f4596e28cb2
wojciech-galan <wojciech.galan@uj.edu.pl>
2018-10-18 16:00:14 +0200

    m

--- code/results_for_student.py
+++ code/results_for_student.py
@@ -10,8 +10,11 @@ def unpickle_file(path):
 
 if __name__ == '__main__':
     how_many_elements = 36
-    data_set_12 = sorted(unpickle_file('res/1.2.dump').items(), key=lambda x:x[1][1]['cl_omega'], reverse=True)[:how_many_elements]
-    print(data_set_12)
+    data_set_12 = sorted(unpickle_file('res/1.2.dump').items(), key=lambda x:x[1][1]['cl_omega'], reverse=True)
+    ids_of_interest_12 = [x[0] for x in data_set_12[:how_many_elements]]
+
+    file_pattern_12 = 'https://github.com/wojciech-galan/msa_exercises/tree/master/%stfa'
+    print([file_pattern_12%x for x in ids_of_interest_12])
     data_set_2 = unpickle_file('res/2.dump')
     data_set_10 = unpickle_file('res/10.dump')
     for x in range(36): # 36 zestawów danych do analizy
